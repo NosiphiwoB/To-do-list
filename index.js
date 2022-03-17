540d6e038792ff6519914af0ced57a26bc9999c4
NosiphiwoB <biyelanosiphiwo@gmail.com>
2022-03-17 19:00:35 +0200

    added onclick function

---
+++ index.js
@@ -0,0 +1,15 @@
+const mySubmit = (event) => {
+  event.preventDefault();
+  let subject = document.getElementById("subject").value;
+  let description = document.getElementById("description").value;
+  let date = document.getElementById("date").value;
+if(subject === "" &&description){
+
+
+
+}else{
+
+    
+}
+  console.log(subject, "Description", description, "Date", date);
+};
