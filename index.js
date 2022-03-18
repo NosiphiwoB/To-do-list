380ecc84b35ed1c5532047e9162944eac99d3c88
NosiphiwoB <biyelanosiphiwo@gmail.com>
2022-03-18 15:53:20 +0200

    To do list

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
+let formData = []
+
 const mySubmit = (event) => {
   event.preventDefault();
   let subject = document.getElementById("subject").value;
   let description = document.getElementById("description").value;
   let date = document.getElementById("date").value;
-if(subject === "" &&description){
 
-
-
-}else{
-
-    
-}
-  console.log(subject, "Description", description, "Date", date);
+  if ((subject, description, date)) {
+    document.getElementById(
+      "msg"
+    ).innerHTML = `<p style="color:green;">Succesfuly submitted</p>`;
+    myDisplay()
+  } else {
+    document.getElementById(
+      "msg"
+    ).innerHTML = `<p style="color:red;">Fill all the required fields</p>`;
+  }
 };
+
+const myDisplay = () => {
+let formDataValue = {subject:subject.value, description:description.value, date:date.value} 
+formData.push(formDataValue)
+localStorage.setItem('user', JSON.stringify(formData));
+ var output = JSON.parse(localStorage.getItem("user"));
+ output.forEach(function(element){
+   document.getElementById("tbody").innerHTML +=`
+   <td>${element.subject}</td>
+   <td>${element.description}</td>
+   <td>${element.date}</td>
+   <td><button onclick="myEdit()">Edit</button>
+   <button onclick="myDelete()">Delete X</button></td>
+
+   
+   ` ;
+   
+ })
+
+
+}
