7eef5277d37d6f896c7369cc015433fbe4fc0862
NosiphiwoB <biyelanosiphiwo@gmail.com>
2022-03-25 15:52:51 +0200

    To do list

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-
-
+var records =[]
 const mySubmit = (event) => {
   event.preventDefault();
   let subject = document.getElementById("subject").value;
@@ -10,6 +9,7 @@ const mySubmit = (event) => {
     document.getElementById(
       "msg"
     ).innerHTML = `<p style="color:green;">Succesfuly submitted</p>`;
+    records.push(subject)
     myDisplay();
     reset();
   } else {
@@ -19,28 +19,76 @@ const mySubmit = (event) => {
   }
 };
 
-const myDelete = (event) => {
-  let element = document.getElementById("tbl");
-  element.parentNode.removeChild(element);
+const myDelete = (id) => {
+  let element = document.getElementById(id);
+  element.remove();
 };
 
-const reset =() => {
+const reset = () => {
   document.getElementById("form").reset();
+};
+
+function save(id){
+  const subject = document.getElementById("edit_subject").value;
+  const description = document.getElementById("edit_description").value;
+  const date = document.getElementById("edit_date").value;
+
+  document.getElementById("subject_"+id).innerHTML=subject;
+  document.getElementById("description_"+id).innerHTML=description;
+  document.getElementById("date_"+id).innerHTML=date;
+  document.getElementById("editform").style.display ="none";
+
 }
 
+function cancel(){
+  document.getElementById("editform").style.display ="none";
+}
 
+const myEdit = (id) => {
+  const subject = document.getElementById("subject_"+id).innerHTML;
+  const description = document.getElementById("description_"+id).innerHTML;
+  const date = document.getElementById("date_"+id).innerHTML;
+  
+  document.getElementById("editform").style.display ="block";
+  document.getElementById("editform").innerHTML =`
+  <h1 id="header">Edit</h1>
+      <div>
+        <label>Subject</lsubjectabel>
+        <input id="edit_subject" type="text" value="${subject}" />
+      </div>
+      <div>
+        <label>Description</label>
+        <input id="edit_description" type="text" value="${description}" />
+      </div>
+      <div>
+        <label>Date</label>
+        <input id="edit_date" type="date" value="${date}" />
+      </div>
+      <br />
+      <br />
+      <div>
+      <button onclick="cancel()">cancel</button>
+    </div>
+      <div>
+        <button onclick="save(${id})">Save</button>
+      </div>
+  `
+
+}
+var id = 0;
 const myDisplay = () => {
-   document.getElementById("tbody").innerHTML +=`
-  <tr id="tbl"> 
-  <td>${subject.value}</td>
-   <td>${description.value}</td>
-   <td>${date.value}</td>
-   <td><button onclick="myEdit()">Edit</button>
-   <button onclick="myDelete(event)">Delete</button>
+  let copy = subject ;
+  console.log(copy)
+
+  document.getElementById("tbody").innerHTML += `
+  <tr id="${id}"> 
+   <td id="subject_${id}">${subject.value}</td>
+   <td id="description_${id}">${description.value}</td>
+   <td id="date_${id}">${date.value}</td>
+   <td><button onclick="myEdit(${id})" id="edit"></button>
+   <button onclick="myDelete(${id})" id="delete"></button>
    </td>
    </tr>
-   ` ;
-   
- }
-
-
+   `;
+   id++;
+};
